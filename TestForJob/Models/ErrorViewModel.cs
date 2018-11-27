7af0c5b57a9d7810a3acb59df86c9cad61124ad2
Yuriy Oleynikov <yuriyoleynikov@hotmail.com>
2018-11-27 17:13:51 +0400

    Add project files.

---
+++ TestForJob/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace TestForJob.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
